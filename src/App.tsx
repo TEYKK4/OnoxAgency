50ff1be013f37e84a4a4a1070eac85f970320ad5
TEYKK4 <95916470+TEYKK4@users.noreply.github.com>
2025-06-10 16:35:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Preview from "./components/HeroSection.tsx";
 import Footer from "./components/Footer.tsx";
-import Header from "./components/Header.tsx";
 import Faq from "./components/Faq.tsx";
 import About from "./components/About.tsx";
 
